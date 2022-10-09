4ffd168f79b646d3565f3380677d8a3022d97fa7
GitHub Action <action@github.com>
2022-10-09 03:18:13 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "idea-demo-2022"
--- src/main/kotlin/com/github/cnzhujie/ideademo2022/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cnzhujie/ideademo2022/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cnzhujie.ideademo2022.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cnzhujie.ideademo2022.MyBundle
 
 class MyApplicationService {
 
